753e4282405f05177e62a54efbb446a787d26831
yanjixiong <yjk99@qq.com>
2015-06-19 10:52:07 +0800

    初始版，可打开页面

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cnode-search-cli
 Command Line for search topics on Cnodejs.org from baidu
+
+# Installation
+```
+npm install -g cnode-search-cli
+```
+
+# Usage
+```
+  Usage: cnode-search <keyword>
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+```
+# License
+MIT
--- bin/cli.js
+++ bin/cli.js
@@ -8,23 +8,11 @@ var appInfo = require('./../package.json');
 
 program
   .version(appInfo.version)
-  .usage('[options] <keyword>')
-//  .option('-n, --npm', 'open document page in npmjs.com')
-//  .option('-g, --github', 'open document page in github.com')
+  .usage(' <keyword>')
   .parse(process.argv);
 
 if (program.args.length != 1) {
   program.help();
 }
-
-//var site = 'default';
-//
-//if (program.npm) {
-//  site = 'npmjs';
-//}
-//
-//if (program.github) {
-//  site = 'github';
-//}
 
 search(program.args[0]);
